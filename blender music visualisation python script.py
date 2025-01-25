af89eab1ed98a1c0ea56a19e47355a58d9fbfff5
Sören Metje <soerenmetje@yahoo.de>
2025-01-25 19:50:27 +0100

    Formatted code

--- blender music visualisation python script.py
+++ blender music visualisation python script.py
@@ -4,44 +4,40 @@ import os.path
 
 start_time = time.time()
 
-
-
 # Number of cubes
 collums = 80
-rows = 5 # rows must be uneven
+rows = 5  # rows must be uneven
 
-mid = int((rows +1)/2)
+mid = int((rows + 1) / 2)
 
 # Max cube height
 maxheight = 6
 
 # Frequency range of whole visualizer
-spectrum_Start = 10 # in Hz
-spectrum_End = 21000 # in Hz
+spectrum_Start = 10  # in Hz
+spectrum_End = 21000  # in Hz
 
 # Path to music file
 path = os.path.abspath("C:\\Users\myusername\Desktop\music.mp3")
 
-
 l = 1
 h = spectrum_Start
 
-base = ( spectrum_End / spectrum_Start ) ** ( 1 / collums )
+base = (spectrum_End / spectrum_Start) ** (1 / collums)
 
-
-
-print("STARTED (Cubes_Spectrum=" + str(collums) + ", Cubes_Vertical=" + str(rows) + ", Cube_Height=" + str(maxheight) + ", Cube_Count=" + str(collums*rows) + ")")
+print("STARTED (Cubes_Spectrum=" + str(collums) + ", Cubes_Vertical=" + str(rows) + ", Cube_Height=" + str(
+    maxheight) + ", Cube_Count=" + str(collums * rows) + ")")
 
 bpy.ops.screen.frame_jump(end=False)
 
-def funktion(x, y, zmax, lo, hi):
 
-    bpy.ops.mesh.primitive_cube_add(location = (x, y, 0))
+def funktion(x, y, zmax, lo, hi):
+    bpy.ops.mesh.primitive_cube_add(location=(x, y, 0))
     bpy.context.scene.cursor_location = bpy.context.active_object.location
     bpy.context.scene.cursor_location.z -= 1
     bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
     ####
-	#to have a gap between the cubes#
+    # to have a gap between the cubes#
     bpy.context.active_object.scale.x = 0.4
     bpy.context.active_object.scale.y = 0.4
     bpy.context.active_object.scale.z = zmax
@@ -51,38 +47,32 @@ def funktion(x, y, zmax, lo, hi):
     bpy.context.active_object.animation_data.action.fcurves[0].lock = True
     bpy.context.active_object.animation_data.action.fcurves[1].lock = True
 
-
     bpy.context.area.type = 'GRAPH_EDITOR'
 
-
     bpy.ops.graph.sound_bake(filepath=path, low=lo, high=hi)
-
-
 
     bpy.context.active_object.animation_data.action.fcurves[2].lock = True
 
 
-
 for i in range(0, collums):
 
-	# exponetial course of frequency
+    # exponential course of frequency
     l = h
     h = round(spectrum_Start * base ** (i + 1), 2)
 
-
     print("     c: " + str(i) + "   l: " + str(l) + " Hz    h: " + str(h) + " Hz")
 
-	# exponetial drop to sides
+    # exponetial drop to sides
     for n in range(0, mid):
-        hoehe = maxheight * (10**(-1*(n/(mid-1))))
+        hoehe = maxheight * (10 ** (-1 * (n / (mid - 1))))
         funktion(n, i, hoehe, l, h)
 
     for n in range(1, mid):
-        hoehe = maxheight * (10**(-1*(n/(mid-1))))
-        funktion((-1*n), i, hoehe, l, h)
+        hoehe = maxheight * (10 ** (-1 * (n / (mid - 1))))
+        funktion((-1 * n), i, hoehe, l, h)
 
-    print(str(round(((i+1)/collums)*100, 1)) + "%   done in " + (str(round((time.time() - start_time)/60, 2))) + " minutes")
-
+    print(str(round(((i + 1) / collums) * 100, 1)) + "%   done in " + (
+        str(round((time.time() - start_time) / 60, 2))) + " minutes")
 
 # change it back to the text editor
 bpy.context.area.type = 'TEXT_EDITOR'
